50079192ee4f56877523f78ab4b53fcbe80d04c1
vidit <136428327+Vidit93@users.noreply.github.com>
2024-02-11 03:23:44 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const user_routes = require("./routes/user");
 const connectDB = require("./db/connect");
+
+app.use(cors());
 
 app.get("/",(req,res)=> {
     res.send("hello ji");
